d9a9de67c1b9673698583437e78b9cb319b301d2
Tim Steeman <tmdev82@gmail.com>
2022-09-25 15:36:03 +0200

    basic frontend implementation

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,25 @@
+use yew::prelude::*;
+
+struct App {}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {}
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+            {"Ethereum transactions viewer frontend"}
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
 }
