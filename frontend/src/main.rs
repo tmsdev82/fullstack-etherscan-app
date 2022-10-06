0d082afacba8c64a13b63df4561f5692ca5f3548
Tim Steeman <tmdev82@gmail.com>
2022-10-06 22:11:53 +0200

    implement ui for retrieving and displaying account data

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,22 +1,26 @@
+use common::models as c_models;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 mod service;
 
 pub enum Msg {
     GetData,
-    ProcessData(Option<String>),
+    ProcessData(Option<c_models::AccountData>),
 }
 
 struct App {
-    received_data: Option<String>,
+    input_ref: NodeRef,
+    received_data: Option<c_models::AccountData>,
 }
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self {
+            input_ref: NodeRef::default(),
             received_data: None,
         }
     }
@@ -25,11 +29,26 @@ impl Component for App {
         match msg {
             Msg::GetData => {
                 log::debug!("Get data");
+                let input = self.input_ref.cast::<HtmlInputElement>().unwrap();
+                let address = if input.value().trim().is_empty() {
+                    "".to_string()
+                } else {
+                    input.value().trim().to_string()
+                };
+
+                input.set_value("");
+                let params = c_models::QueryParams {
+                    address: address,
+                    page: 1,
+                    offset: 1000,
+                    sort: "asc".to_string(),
+                };
+
                 ctx.link().send_future(async move {
-                    let result = service::get_data().await;
+                    let result = service::get_account_data(&params).await;
 
                     Msg::ProcessData(result)
-                })
+                });
             }
             Msg::ProcessData(data) => {
                 log::debug!("Received data: {:?}", data);
@@ -50,16 +69,65 @@ impl Component for App {
             <div>
             <h1>{"Ethereum transactions viewer frontend"}</h1>
             <div>
-            <button onclick={on_click}>{"Get Data"}</button>
-            </div>
-            <div>
+                    <input style="width:350px;" type="text" ref={self.input_ref.clone()} />
+                    <button onclick={on_click}>{"Get Account Data"}</button>
+                </div>
+                <div>
                 { match self.received_data.clone() {
-                    Some(data) => data,
-                    _ => "No data received".to_string()
+                    Some(val) => {
+                        show_account(&val)
+                    },
+                    _ => html! {
+                        <div>
+                            {"Please enter an address"}
+                        </div>
+                    }
                 }}
-            </div>
+                </div>
             </div>
         }
+    }
+}
+
+fn show_account(account: &c_models::AccountData) -> Html {
+    let normal_tx = account.normal_transactions.clone();
+    let tx_html = normal_tx.into_iter().map(|tx| {
+        html! {
+            <tr key={tx.hash}>
+                <td>{tx.from}</td>
+                <td>{tx.value}</td>
+                <td>{tx.gas_price}</td>
+                <td>{tx.contract_address}</td>
+                <td>{tx.function_name}</td>
+            </tr>
+        }
+    });
+
+    html! {
+        <div>
+            <div>
+            { format!("Address: {}, Balance: {}, Normal tx: {}",
+            account.address, account.balance, account.normal_transactions.len()
+            ) }
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                        <th>{"From"}</th>
+                        <th>{"Value"}</th>
+                        <th>{"Gas Price"}</th>
+                        <th>{"Contract"}</th>
+                        <th>{"Function"}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {for tx_html}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     }
 }
 
